05a65be7f4881d725fa925bd723fd4f212b17b04
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-02-28 18:22:32 +0300

    Added CustomUser model.

---
+++ back/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+
+
+class CustomUser(AbstractUser):
+    cities = (
+        ('MSC', 'Москва'),
+        ('SPB', 'Санкт-Петербург'),
+        ('PRM', 'Пермь'),
+        ('NNV', 'Нижний Новгород'),
+    )
+    telephone_number = PhoneNumberField(null=True, blank=False, unique=True)
+    age = models.IntegerField(null=False, blank=False)
+    gender = models.BooleanField() # true for male, false for female
+    city = models.CharField(choices=cities, max_length=255)
+
