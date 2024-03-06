a7a76f53fb98e059aa5fe91f80dd6ebdd753303a
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-03-07 00:31:27 +0300

    Second step to resolve stash conflict.

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -20,20 +20,6 @@ function App() {
     fetchCSRFToken()
   }, [])
 
-  // получаем csrftoken для POST, PUT, PATCH и  DELETE запросов и записываем его в куки.
-  useEffect(() => {
-    const fetchCSRFToken = async () => {
-      Axios.get('get-csrf-token/')
-      .then((response) => {
-        console.log('cookie set')
-        Cookies.set('csrftoken', response.data.csrf_token)
-        Axios.defaults.headers.common['X-CSRFToken'] = Cookies.get('csrftoken')
-      })
-    }
-
-    fetchCSRFToken()
-  }, [])
-
   return (
     <>
       <AppRoutes></AppRoutes>
