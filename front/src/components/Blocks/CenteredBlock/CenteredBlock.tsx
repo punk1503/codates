cb1c54632e94d7da9df835e3c88d724dead1e72d
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-03-10 12:56:00 +0300

    Finished main page layout.

--- front/src/components/Blocks/CenteredBlock/CenteredBlock.tsx
+++ front/src/components/Blocks/CenteredBlock/CenteredBlock.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from "react";
 import "./CenteredBlock.css"
 
-export default function CenteredBlock({children} : {children: ReactNode}) {
+export default function CenteredBlock({children, style} : {children: ReactNode, style?: React.CSSProperties }) {
     return (
-        <div className="centered_block">
+        <div className="centered_block" style={style}>
             {children}
         </div>
     )
