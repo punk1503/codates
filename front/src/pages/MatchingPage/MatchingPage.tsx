205ce56a78d7b2a4805f1c130740be354906adce
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-03-10 13:14:38 +0300

    Added user card and highlight.js to repository.

---
+++ front/src/pages/MatchingPage/MatchingPage.tsx
@@ -0,0 +1,13 @@
+import "./MatchingPage.css"
+import UserCard from "../../components/UserCard"
+
+export default function MatchingPage() {
+    return (
+        <>
+            <h1>Matching</h1>
+            <UserCard>
+                
+            </UserCard>
+        </>
+    )
+}
