d7c7992dd946c7016392751e56450234c81b14f4
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-03-13 19:37:21 +0300

    Corrected left and right photo buttons color gradient.

--- front/src/pages/MainPage/MainPage.tsx
+++ front/src/pages/MainPage/MainPage.tsx
@@ -1,7 +1,9 @@
 import "../../components/Button/Button.css"
 import "./MainPage.css"
 import { CenteredBlock } from "../../components/Blocks"
+import { useAuth } from "../../context/AuthContext"
 export default function MainPage() {
+    const { isAuthenticated } = useAuth()
     return (
         <>
         <CenteredBlock style={{marginTop: "5rem"}}>
@@ -12,10 +14,12 @@ export default function MainPage() {
                             <h1><span style={{color: "var(--primary)"}}>Co</span><span style={{color: "var(--accent)"}}>Dates</span></h1>
                             <p>Ваш помощник в корпоративном нетворкинге.</p>
                         </div>
-                        <div className="main_page__grid__wrapper--buttons">
-                            <a href="/signin" className="custom_button">Войти</a>
-                            <span>Еще нет аккаунта?<br /><a href="/signup">Регистрация</a></span>
-                        </div>
+                        {isAuthenticated ? 
+                            <div className="main_page__grid__wrapper--buttons">
+                                <a href="/signin" className="custom_button">Войти</a>
+                                <span>Еще нет аккаунта?<br /><a href="/signup">Регистрация</a></span>
+                            </div>
+                        : <></>}
                     </div>
                     <div className="main_page__grid__col">
                         <img src="https://placehold.co/400x400" alt="" />
