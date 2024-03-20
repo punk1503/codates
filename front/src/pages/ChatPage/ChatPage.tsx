7704fe11be7b115cd91a5d348ac139a758fbe5cd
Zaharov Andrey <zaharov.andrey161@gmail.com>
2024-03-20 17:18:28 +0300

    Added chat styles and scrolling on new message.

--- front/src/pages/ChatPage/ChatPage.tsx
+++ front/src/pages/ChatPage/ChatPage.tsx
@@ -1,10 +1,23 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { useParams } from 'react-router-dom'
 import { CenteredBlock } from "../../components/Blocks"
 import { Message } from '../../types/Chats.interface'
 import { backendAdress } from "../../utils/env"
+import { User } from "../../types/User.interface"
 import Axios from "../../utils/axiosConfig"
 import Spinner from "../../components/Spinner"
+import './ChatPage.css'
+
+function Msg(props: {text: string, user: User}) {
+    const { user_id } = useParams<{ user_id: string }>()
+    if (user_id) {
+        return (
+            <div className={"message" + (parseInt(user_id) !== props.user.id ? " message--right" : " message--left")}>{props.text}</div>
+        )
+    }
+
+    return <></>
+}
 
 export default function ChatPage() {
     const { user_id } = useParams<{ user_id: string }>()
@@ -13,12 +26,12 @@ export default function ChatPage() {
     const socketUrl = `ws/chat/${user_id}/`
     const [socket, setSocket] = useState<WebSocket | null>(null)
     const [readyState, setReadyState] = useState<number>(WebSocket.CLOSED);
+    const chatDummyRef = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
         function listener(event: any) {
             const responseData: Message = JSON.parse(event.data).message
             setMessages(prevMessages => [...prevMessages, responseData])
-            console.log(responseData)
         }
         const newSocket = new WebSocket('ws' + backendAdress.slice(4) + socketUrl)
         setReadyState(WebSocket.CONNECTING)
@@ -31,6 +44,8 @@ export default function ChatPage() {
             newSocket.close()
         }
     }, [user_id])
+
+    useEffect(() => {if (chatDummyRef.current) chatDummyRef.current.scrollIntoView({behavior: 'smooth'})}, [messages])
 
     useEffect(() => {
         Axios.get(`chat/${user_id}/`)
@@ -55,15 +70,20 @@ export default function ChatPage() {
     return (
         <CenteredBlock>
             {readyState === WebSocket.OPEN ?
-            <>
-                {messages.map((message, index) => (
-                    <div key={index}>{message.user.first_name}: {message.text}</div>
-                ))}
-                <div>
-                    <input value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} type="text" />
-                    <button onClick={sendMessage}>Отправить</button>
+            <div className="chat_block">  
+                <div className="messages">
+                    {messages.map((message, index) => (
+                        <Msg key={index} text={message.text} user={message.user}></Msg>
+                    ))}
+                    <div ref={chatDummyRef} id="dummy"></div>
                 </div>
-            </> 
+                <div className="send_block">
+                    <input className="message_input" value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} type="text" />
+                    <button className="message_send_button" onClick={sendMessage}>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24"><path d="M120-160v-640l760 320-760 320Zm80-120 474-200-474-200v140l240 60-240 60v140Zm0 0v-400 400Z"/></svg>
+                    </button>
+                </div>
+            </div> 
             :
             <>
                 <h1>Чат не существует</h1>
--- front/src/pages/ChatsPage/ChatsPage.tsx
+++ front/src/pages/ChatsPage/ChatsPage.tsx
@@ -3,6 +3,8 @@ import { CenteredBlock } from "../../components/Blocks"
 import { Chat } from '../../types/Chats.interface'
 import { Link } from "react-router-dom"
 import Axios from "../../utils/axiosConfig"
+import './ChatsPage.css'
+import '../../components/ClearLink.css'
 
 export default function ChatsPage() {
     const [chats, setChats] = useState<Chat[]>([])
@@ -20,13 +22,18 @@ export default function ChatsPage() {
     return (
         <CenteredBlock>
             <h1>Чаты</h1>
-            {chats.map((chat, index) => {
-                return (
-                    <div key={index} className="chat">
-                        <Link to={`/chat/${chat.user.id}`}>{chat.user.first_name}</Link>
-                    </div>
-                )
-            })}
+            <div className="chats">
+                {chats.map((chat, index) => {
+                    return (
+                        <Link className="clear_link" to={`/chat/${chat.user.id}`}>
+                            <img src={chat.user.images[0]?.image} alt="" />
+                            <div key={index} className="chat">
+                                {chat.user.first_name}
+                            </div>
+                        </Link>
+                    )
+                })}
+            </div>
         </CenteredBlock>
     )
 }
